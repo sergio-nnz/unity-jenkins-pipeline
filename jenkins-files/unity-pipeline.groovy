3b2aa5fdf677de748e32f3927b2ade8622020b4b
sergio-nnz <sergio.nnsa@gmail.com>
2018-04-29 23:00:45 -0700

    Added the initial jenkins files in order to start development

---
+++ jenkins-files/unity-pipeline.groovy
@@ -0,0 +1,23 @@
+node ("master") {
+  stage("Pull the project") {
+
+  }
+
+  stage("Unit Tests") {
+
+  }
+
+  stage("Playmode Tests") {
+
+  }
+
+  stage("Generate Project") {
+
+  }
+}
+
+@NonCPS
+def getGitChangeLog()
+{
+  
+}
