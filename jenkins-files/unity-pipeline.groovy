5c823109d96c889c7e69a5ba40d38b92ef39846a
sergio-nnz <sergio.nnsa@gmail.com>
2018-05-01 20:09:34 -0700

    Started adding content to the pipeline and helper

--- jenkins-files/unity-pipeline.groovy
+++ jenkins-files/unity-pipeline.groovy
@@ -1,23 +1,39 @@
 node ("master") {
   stage("Pull the project") {
+    try {
+      // checkout([$class: 'GitSCM', branches:[[name: '*/master']], doGenerateSubmoduleConfiguration: false, extensions: [], submoduleCfg: [], userRemoteConfigs: [[credentialsId: "", url: ""]]])
+    } catch (error) {
 
+    }
   }
 
   stage("Unit Tests") {
+    try {
 
+    } catch (error) {
+
+    }
   }
 
   stage("Playmode Tests") {
+    try {
 
+    } catch (error) {
+
+    }
   }
 
   stage("Generate Project") {
+    try {
 
+    } catch (error) {
+
+    }
   }
 }
 
 @NonCPS
 def getGitChangeLog()
 {
-  
+
 }
